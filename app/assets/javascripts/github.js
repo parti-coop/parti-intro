a2cf8f862542f0b857a8fa85aace5ab7ec15f40b
dali <dali@ufofactory.org>
2016-01-26 08:32:36 +0900

    @레이블 #4

--- app/assets/javascripts/github.js
+++ app/assets/javascripts/github.js
@@ -1,122 +1,162 @@
 (function ($) {
-    'use strict';
+  'use strict';
 
-    $.githubWidget = function(element, options) {
-        var defaults = {
-            user: '',
-            widget: 'issues',
+  $.githubWidget = function(element, options) {
+    var defaults = {
+      user: '',
+      widget: 'assigned',
+    }
+    var plugin = this;
+    plugin.settings = {};
+
+    var $element = $(element),
+    element = element;
+
+    var fillElement = function(title, content) {
+      var panel = '<div class="panel panel-default">';
+      panel += '<div class="panel-heading">' + title + '</div>';
+      panel += content;
+      panel += '</div>';
+      $element.html(panel);
+    };
+
+    var autoTitle = function(footer) {
+      var headingText = plugin.settings.widget.charAt(0).toUpperCase() + plugin.settings.widget.slice(1);
+      return '<a href="' + plugin.settings.headingLink + '" target="_blank">'
+      + headingText +
+      '</a>';
+    };
+
+    var assigneeContent = function(issue) {
+      if(issue.assignee) {
+        return '<div class="pull-right"><img src="' + issue.assignee.avatar_url + '" style="max-width: 20px; margin-left: 5px;"> @' + issue.assignee.login + '</div>';
+      }
+
+      return '';
+    };
+
+    var issuesContent = function(issues) {
+      var content = '<ul class="list-group">';
+
+      $.each(issues, function(k, v) {
+        var repo_matched = v.url.match(/^https:\/\/api\.github\.com\/repos\/(.+)\/issues\/\d+$/);
+        var repo = (repo_matched === null ? '' : repo_matched[1])
+        content += '<li class="list-group-item">';
+        content += '<div class="text-muted clearfix">' + repo + ' #' + v.number + assigneeContent(v) + '</div>';
+        content += '<strong><a href="' + v.html_url + '" target="_blank">' + v.title + '</a></strong><br/>' + v.body;
+        if(v.labels) {
+          $.each(v.labels, function(label_k, label_v) {
+            content += '<span class="label" style="color:' + ((parseInt(label_v.color, 16) > 0xffffff/2) ? 'black':'white') + '; background-color:#' + label_v.color + '">' + label_v.name + '</span>';
+          });
         }
-        var plugin = this;
-        plugin.settings = {};
+      });
 
-        var $element = $(element),
-            element = element;
+      content += '</ul>';
 
-        var fillElement = function(title, content) {
-            var panel = '<div class="panel panel-default">';
-            panel += '<div class="panel-heading">' + title + '</div>';
-            panel += content;
-            panel += '</div>';
-            $element.html(panel);
-        };
-
-        var autoTitle = function(footer) {
-            return '<a href="https://github.com/' + plugin.settings.user + '">'
-                        + plugin.settings.user +
-                    '</a> <span class="text-muted">@ GitHub</span>';
-        };
-
-        var fetchFromGithub = function() {
-            var user = plugin.settings.user;
-
-            $.ajax({
-                url: 'https://api.github.com/search/issues?q=user:parti-xyz+is:open+assignee:' + user + '&sort=pushed',
-                type: 'GET',
-                dataType: 'json',
-                success: function(data) {
-                    var title = autoTitle();
-
-                    var content = '<ul class="list-group">';
-
-                    $.each(data.items, function(k, v) {
-                        var repo_matched = v.url.match(/^https:\/\/api\.github\.com\/repos\/(.+)\/issues\/\d+$/);
-                        var repo = (repo_matched === null ? '' : repo_matched[1])
-                        content += '<li class="list-group-item">';
-                        content += '<div class="text-muted">' + repo + ' #' + v.number +'</div>';
-                        content += '<strong><a href="' + v.html_url + '">' + v.title + '</a></strong><br/>' + v.body;
-                    });
-
-                    content += '</ul>';
-
-                    fillElement(title, content);
-                }
-            });
-        }
-
-        var fetchRepostoriesFromGithub = function() {
-            var user = plugin.settings.user;
-
-            $.ajax({
-                url: 'https://api.github.com/users/parti-xyz/repos?sort=full_name&direction=asc',
-                type: 'GET',
-                dataType: 'json',
-                success: function(data) {
-                    var title = autoTitle();
-
-                    var content = '<ul class="list-group">';
-
-                    content += '<li class="list-group-item list-group-item-warning">';
-                    content += '<div class="text-muted">Sourcing</div>'
-                    content += '</li>'
-                    $.each(data, function(k, v) {
-                        if(v.fork == false) {
-                            content += '<li class="list-group-item">';
-                            content += '<strong><a href="' + v.html_url + '/issues" target="_blank">' + v.name + '</a></strong> (' + v.open_issues_count + ')<br/>' + v.description;
-                            content += '</li>'
-                        }
-                    });
-
-                    content += '<li class="list-group-item list-group-item-warning">';
-                    content += '<div class="text-muted">Forked</div>'
-                    content += '</li>'
-                    $.each(data, function(k, v) {
-                        if(v.fork == true) {
-                            content += '<li class="list-group-item">';
-                            content += '<strong><a href="' + v.html_url + '/issues" target="_blank">' + v.name + '</a></strong> (' + v.open_issues_count + ')<br/>' + v.description;
-                            content += '</li>'
-                        }
-                    });
-
-                    content += '</ul>';
-
-                    fillElement(title, content);
-                }
-            });
-        }
-
-        plugin.init = function() {
-            plugin.settings = $.extend({}, defaults, options);
-            if (plugin.settings.widget === 'issue' && plugin.settings.user !== '') {
-                fetchFromGithub();
-            } else if (plugin.settings.widget === 'repos') {
-                fetchRepostoriesFromGithub();
-            }
-
-        }
-
-        plugin.init();
+      return content;
     }
 
-    $(function() {
-        $.each($('[data-toggle="github-widget"]'), function() {
-            var inputUser = $(this).data('user');
-            var inputWidget = $(this).data('widget');
+    var fetchAssignedFromGithub = function() {
+      var user = plugin.settings.user;
 
-            var options = {};
-            options.user = inputUser;
-            options.widget = (inputWidget !== undefined) ? inputWidget : 'issue';
-            var plugin = new $.githubWidget(this, options);
-        });
-    });
+      $.ajax({
+        url: 'https://api.github.com/search/issues?q=user:parti-xyz+is:open+assignee:' + user + '&sort=pushed',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+          var title = autoTitle();
+          var content = issuesContent(data.items);
+          fillElement(title, content);
+        }
+      });
+    }
+
+    var fetchOnlyLabeledFromGithub = function() {
+      var user = plugin.settings.user;
+
+      $.ajax({
+        url: 'https://api.github.com/search/issues?q=user:parti-xyz+is:open+label:@' + user + '&sort=pushed',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+          var title = autoTitle();
+          var content = issuesContent(data.items.filter(function(v) {
+            return (v.assignee && v.assignee.login == user) ? false : true
+          }));
+          fillElement(title, content);
+        }
+      });
+    }
+
+    var fetchRepostoriesFromGithub = function() {
+      var user = plugin.settings.user;
+
+      $.ajax({
+        url: 'https://api.github.com/users/parti-xyz/repos?sort=full_name&direction=asc',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+          var title = autoTitle();
+
+          var content = '<ul class="list-group">';
+
+          content += '<li class="list-group-item list-group-item-warning">';
+          content += '<div class="text-muted">Sourcing</div>'
+          content += '</li>'
+          $.each(data, function(k, v) {
+            if(v.fork == false) {
+              content += '<li class="list-group-item">';
+              content += '<strong><a href="' + v.html_url + '/issues" target="_blank">' + v.name + '</a></strong> (' + v.open_issues_count + ')<br/>' + v.description;
+              content += '</li>'
+            }
+          });
+
+          content += '<li class="list-group-item list-group-item-warning">';
+          content += '<div class="text-muted">Forked</div>'
+          content += '</li>'
+          $.each(data, function(k, v) {
+            if(v.fork == true) {
+              content += '<li class="list-group-item">';
+              content += '<strong><a href="' + v.html_url + '/issues" target="_blank">' + v.name + '</a></strong> (' + v.open_issues_count + ')<br/>' + v.description;
+              content += '</li>'
+            }
+          });
+
+          content += '</ul>';
+
+          fillElement(title, content);
+        }
+      });
+}
+
+plugin.init = function() {
+  plugin.settings = $.extend({}, defaults, options);
+  if (plugin.settings.widget === 'assigned' && plugin.settings.user !== '') {
+    plugin.settings.headingLink = "https://github.com/issues?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aparti-xyz+assignee%3A" + plugin.settings.user;
+    fetchAssignedFromGithub();
+  } else if (plugin.settings.widget === 'only-labeled' && plugin.settings.user !== '') {
+    plugin.settings.headingLink = "https://github.com/issues?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+user%3Aparti-xyz+label%3A@" + plugin.settings.user;
+    fetchOnlyLabeledFromGithub();
+  } else if (plugin.settings.widget === 'repositories') {
+    plugin.settings.headingLink = "https://github.com/parti-xyz";
+    fetchRepostoriesFromGithub();
+  }
+
+}
+
+plugin.init();
+}
+
+$(function() {
+  $.each($('[data-toggle="github-widget"]'), function() {
+    var inputUser = $(this).data('user');
+    var inputWidget = $(this).data('widget');
+
+    var options = {};
+    options.user = inputUser;
+    options.widget = (inputWidget !== undefined) ? inputWidget : 'assigned';
+    var plugin = new $.githubWidget(this, options);
+  });
+});
 
 })(jQuery);
